4d0b4cae8f2de170d1f0a05edf3d6302f44211d5
Kyian Vitaliy <chebutalio@gmai.com>
2019-12-11 21:32:36 +0200

    Updates

---
+++ precache-manifest.9ae032e4873857f0af189d8a39c9e56d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c172128ae8441d54ce80d3169214364e",
+    "url": "/Homework_5-JS/index.html"
+  },
+  {
+    "revision": "3232cf8a588cca5a4e26",
+    "url": "/Homework_5-JS/static/css/main.b9161d89.chunk.css"
+  },
+  {
+    "revision": "fa6d3fdebf63eb2e3a49",
+    "url": "/Homework_5-JS/static/js/2.2b7c256d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Homework_5-JS/static/js/2.2b7c256d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3232cf8a588cca5a4e26",
+    "url": "/Homework_5-JS/static/js/main.4e7ed20c.chunk.js"
+  },
+  {
+    "revision": "79f47f882a9a4341c034",
+    "url": "/Homework_5-JS/static/js/runtime-main.b0a474e9.js"
+  },
+  {
+    "revision": "11516e271ba7c68e7aa5cddf61e0bcdd",
+    "url": "/Homework_5-JS/static/media/cat.11516e27.jpg"
+  }
+]);
